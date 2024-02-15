becaeea6022957e1606f21f5889b860bec7bd98f
Phantom-AL <den2005den0715@gmail.com>
2024-02-15 16:18:32 +0200

    Created model manager

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,13 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset() \
+            .filter(status=Post.Status.PUBLISHED)
 
+
+class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
@@ -18,6 +23,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
+    objects = models.Manager()
+    published = PublishedManager()
+
     class Meta:
         ordering = ['-publish']
         indexes = [
@@ -26,4 +34,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
